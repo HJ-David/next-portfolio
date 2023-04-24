4cda2c92ea72060229565a6ab5593b1eb8b2b49c
HJ-David <david.hoonjin.jang@gmail.com>
2023-04-23 18:44:51 -0700

    fifth commit: mobile navbar size adjudsted

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -124,7 +124,7 @@ const NavBar = () => {
           >
             <BsGithub className='text-2xl' />
           </motion.a>
-          <motion.a
+          {/* <motion.a
             href='mailto:david.hoonjin.jang@gmail.com'
             target={"_blank"}
             className='w-6 mx-3'
@@ -132,7 +132,7 @@ const NavBar = () => {
             whileTap={{ scale: 0.9 }}
           >
             <TfiEmail className='text-2xl' />
-          </motion.a>
+          </motion.a> */}
 
           <button
             onClick={() => setMode(mode === "light" ? "dark" : "light")}
@@ -151,10 +151,10 @@ const NavBar = () => {
 
       {isOpen ? (
         <motion.div
-          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          initial={{ scale: 0, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
-          className='min-w-[70vw] sm:min-w-[90vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-      bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
+          className='min-w-[30vw] sm:min-w-[40vw] flex flex-col justify-between z-30 items- fixed top-[80px] left-[30px] -translate-x-1/2 -translate-y-1/2
+      bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-10
       '
         >
           <nav className='flex items-center flex-col justify-center'>
@@ -203,7 +203,7 @@ const NavBar = () => {
             >
               <BsGithub className='text-2xl' />
             </motion.a>
-            <motion.a
+            {/* <motion.a
               href='mailto:david.hoonjin.jang@gmail.com'
               target={"_blank"}
               className='w-6 mx-3  rounded-full sm:mx-1'
@@ -211,7 +211,7 @@ const NavBar = () => {
               whileTap={{ scale: 0.9 }}
             >
               <TfiEmail className='text-2xl dark:text-dark text-white' />
-            </motion.a>
+            </motion.a> */}
 
             <button
               onClick={() => setMode(mode === "light" ? "dark" : "light")}
